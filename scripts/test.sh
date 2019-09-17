c0fa3af14b77216efdfd2e91781f29f52c034e2b
Trevor Robinson <trobinson@glgroup.com>
2019-09-17 10:34:55 -0500

    Verbose curl when waiting for FusionAuth

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,9 +4,9 @@ set -e
 
 docker-compose up --build -d
 
-while ! curl --output /dev/null --silent --head --fail http://localhost:9011; do
+while ! curl -v --output /dev/null --silent --head --fail http://localhost:9011/; do
   echo "Waiting for FusionAuth"
-  sleep 1
+  sleep 5
 done
 
 $(dirname $BASH_SOURCE)/configure.sh
