479c5ecfa8354b5c79c404481df5a889dbc0275e
Trevor Robinson <trevor@loancrate.com>
2022-11-17 11:50:34 -0600

    Fix test error code reporting

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,11 @@ set -e
 
 docker-compose up --build -d
 rm -rf coverage test-results
-docker exec -t express-jwt-fusionauth_app ./scripts/run.sh || true
-docker cp express-jwt-fusionauth_app:/home/node/app/coverage coverage
-docker cp express-jwt-fusionauth_app:/home/node/app/test-results test-results
+
+saveResults() {
+  docker cp express-jwt-fusionauth_app:/home/node/app/coverage coverage
+  docker cp express-jwt-fusionauth_app:/home/node/app/test-results test-results
+}
+
+trap saveResults EXIT
+docker exec -t express-jwt-fusionauth_app ./scripts/run.sh
